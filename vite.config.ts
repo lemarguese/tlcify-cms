c37830effe8f58bdd5776b7d59cc647fcd9ce9e1
Le Marguesse <amargesono@gmail.com>
2025-07-18 20:17:10 -0400

    fix: (domain for admin changed).

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
   },
   server: {
     host: true,
-    allowedHosts: ['tlcify.com', 'www.tlcify.com']
+    allowedHosts: ['admin.tlcify.com', 'www.admin.tlcify.com']
   }
 })
