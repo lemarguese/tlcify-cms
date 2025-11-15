46bd59282b8282889c1666f263df30e1f88a67c5
Le Marguesse <amargesono@gmail.com>
2025-11-15 17:35:29 -0500

    fix: (axios updated)

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -16,8 +16,8 @@ instance.interceptors.request.use(config => {
   const parts = hostname.split(".");
   const tenant = parts.length > 2 ? parts[0] : null; // "samkara"
 
-  config.headers["X-Tenant-Id"] = 'localhost';
-  // if (tenant) config.headers["X-Tenant-Id"] = tenant;
+  // config.headers["X-Tenant-Id"] = 'localhost';
+  if (tenant) config.headers["X-Tenant-Id"] = tenant;
 
   if (!pendingToastId) pendingToastId = toast.loading('Working on it...');
 
